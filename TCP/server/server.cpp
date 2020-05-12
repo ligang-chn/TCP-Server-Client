0c0592daff4368f2faad06aa8ba53ecf4d8885d0
ligang-chn <ligang.she@gmail.com>
2020-05-12 18:59:19 +0800

    release_v1.0

--- TCP/server/server.cpp
+++ TCP/server/server.cpp
@@ -5,6 +5,9 @@
 #pragma comment(lib,"ws2_32.lib")
 #include <iostream>
 #include<vector>
+#include<string>
+#include<algorithm>
+#include<sstream>
 using namespace std;
 
 //enum NAME {
@@ -22,11 +25,8 @@ struct DataHeader {
 };
 
 //DataPackage
-struct DataPackage : public DataHeader
+struct DataPackage
 {
-	DataPackage() {
-		dataLength = sizeof(DataPackage);
-	}
 	char msgCode[8];//电文号
 	char sendTime[14];//发送时间
 	char bfno;//高炉号
@@ -34,34 +34,100 @@ struct DataPackage : public DataHeader
 	char data1[400];
 	char ledNo2;//大屏号2
 	char data2[400];
+	char ledNo3;//大屏号3
+	char data3[400];
 };
 
+struct DataStr
+{
+	string msgCode;//电文号
+	string sendTime;//发送时间
+	char bfno;//高炉号
+	char ledNo1;//大屏号1
+	string data1;
+	char ledNo2;//大屏号2
+	string data2;
+	char ledNo3;//大屏号3
+	string data3;
+	
+};
+
+vector<string> split3(const string &str, const char pattern)
+{
+	vector<string> res;
+	stringstream ss(str);   //读取str到字符串流中
+	string temp;
+	//使用getline函数从字符串流中读取,遇到分隔符时停止,和从cin中读取类似
+	//注意,getline默认是可以读取空格的
+	while (getline(ss, temp, pattern))
+	{
+		res.push_back(temp);
+	}
+	return res;
+}
 
 std::vector<SOCKET> g_clients;
 
 int processor(SOCKET _cSock) {
 	//缓冲区
-	char szRecv[1024] = {};
-	//5)接收客户端数据
-	int nLen = recv(_cSock, szRecv, sizeof(DataHeader), 0);
-	DataHeader* header = (DataHeader*)szRecv;
+	char szRecv[1500] = {};
+	//5)接收客户端数据,首先接收数据长度
+	//int nLen = recv(_cSock, szRecv, sizeof(DataHeader), 0);
+	//DataHeader* header = (DataHeader*)szRecv;
+	int nLen = recv(_cSock, szRecv, sizeof(DataPackage), 0);
 	if (nLen <= 0) {
 		std::cout << "客户端已退出，任务结束" << endl;
 		return -1;
 	}
-	
+	//char数组转换成string，方便处理
+	string recvStr = szRecv;
+	DataStr recvData;
+	recvData.msgCode = recvStr.substr(0, 8);
+	recvData.msgCode.erase(recvData.msgCode.find_last_not_of(" ") + 1);
+	recvData.sendTime = recvStr.substr(8, 14);
+	recvData.bfno = recvStr[22];
+	recvData.ledNo1 = recvStr[23];
+	recvData.data1 = recvStr.substr(24, 400);
+	//去除尾部空符串
+	recvData.data1.erase(recvData.data1.find_last_not_of(" ") + 1);
+	recvData.ledNo2 = recvStr[424];
+	recvData.data2 = recvStr.substr(425, 400);
+	recvData.data2.erase(recvData.data2.find_last_not_of(" ") + 1);
+	recvData.ledNo3 = recvStr[825];
+	recvData.data3 = recvStr.substr(826, 400);
+	recvData.data3.erase(recvData.data3.find_last_not_of(" ") + 1);
 
-	recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-	DataPackage *dataPackage= (DataPackage*)szRecv;
-	std::cout << "收到数据长度：" << dataPackage->dataLength << endl;
-	std::cout << "socket = " << _cSock<<",数据如下：---------" << endl;
-	std::cout << "--电文号：" << dataPackage->msgCode << endl;
-	std::cout << "--发送时间：" << dataPackage->sendTime << endl;
-	std::cout << "--高炉号：" << dataPackage->bfno << endl;
-	std::cout << "--大屏1：" << dataPackage->ledNo1 << endl;
-	std::cout << "--数据1：" << dataPackage->data1 << endl;
-	std::cout << "--大屏2：" << dataPackage->ledNo2 << endl;
-	std::cout << "--数据2：" << dataPackage->data2 << endl;
+	vector<string> resStr;//接收数据区分割之后的字符串
+	vector<pair<string, string> > bufferData1;
+	resStr=split3(recvData.data1, '|');//分割字符串
+	for (vector<string>::iterator it = resStr.begin(); it != resStr.end();it+=2) {
+		bufferData1.push_back(make_pair(*it, *(it + 1)));//存储到缓存区，方便后续使用
+	}
+	resStr.clear();
+	vector<pair<string, string> > bufferData2;
+	resStr = split3(recvData.data2, '|');//分割字符串
+	for (vector<string>::iterator it = resStr.begin(); it != resStr.end(); it += 2) {
+		bufferData2.push_back(make_pair(*it, *(it + 1)));//存储到缓存区，方便后续使用
+	}
+	resStr.clear();
+	vector<pair<string, string> > bufferData3;
+	resStr = split3(recvData.data3, '|');//分割字符串
+	for (vector<string>::iterator it = resStr.begin(); it != resStr.end(); it += 2) {
+		bufferData3.push_back(make_pair(*it, *(it + 1)));//存储到缓存区，方便后续使用
+	}
+
+
+	//recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+	//DataPackage *dataPackage= (DataPackage*)szRecv;
+	//std::cout << "收到数据长度：" << dataPackage->dataLength << endl;
+	//std::cout << "socket = " << _cSock<<",数据如下：---------" << endl;
+	//std::cout << "--电文号：" << dataPackage->msgCode << endl;
+	//std::cout << "--发送时间：" << dataPackage->sendTime << endl;
+	//std::cout << "--高炉号：" << dataPackage->bfno << endl;
+	//std::cout << "--大屏1：" << dataPackage->ledNo1 << endl;
+	//std::cout << "--数据1：" << dataPackage->data1 << endl;
+	//std::cout << "--大屏2：" << dataPackage->ledNo2 << endl;
+	//std::cout << "--数据2：" << dataPackage->data2 << endl;
 	std::cout << "---------单次接收完成" << endl;
 
 	return 0;
